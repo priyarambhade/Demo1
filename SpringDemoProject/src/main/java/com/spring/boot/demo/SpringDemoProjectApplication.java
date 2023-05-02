84533ad3d731bd095cc0b1dba30868a4a273704e
Priya Rambhade <priya.rambhade@prasko.in>
2023-05-02 23:23:21 +0530

    initial code

---
+++ SpringDemoProject/src/main/java/com/spring/boot/demo/SpringDemoProjectApplication.java
@@ -0,0 +1,14 @@
+package com.spring.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println();
+		SpringApplication.run(SpringDemoProjectApplication.class, args);
+	}
+
+}
